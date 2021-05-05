ce2f511f5a243298a32575ccd3e2932d0ae48897
Maco-coder <mamoranl@uwaterloo.ca>
2021-05-05 10:04:07 -0400

    Update Test1.cs

--- Basics/Basics_project/Assets/Scripts/Test1.cs
+++ Basics/Basics_project/Assets/Scripts/Test1.cs
@@ -4,10 +4,16 @@ using UnityEngine                ;
 
 public class Test1 : MonoBehaviour
 {
+
+    Rigidbody body;
+
     // Start is called before the first frame update
     void Start()
     {
         // Destroy(gameObject,2);
+
+        body = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
