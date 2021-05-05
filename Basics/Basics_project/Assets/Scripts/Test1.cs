c08f0d2f92d6fcd0c902d8337f79be0a57249443
Maco-coder <mamoranl@uwaterloo.ca>
2021-05-05 11:27:44 -0400

    Tutorial completed

--- Basics/Basics_project/Assets/Scripts/Test1.cs
+++ Basics/Basics_project/Assets/Scripts/Test1.cs
@@ -1,11 +1,20 @@
 using System.Collections         ;
 using System.Collections.Generic ;
 using UnityEngine                ;
+using UnityEngine.SceneManagement;
+
 
 public class Test1 : MonoBehaviour
 {
 
-    Rigidbody body;
+    Rigidbody body            ;
+    
+    public GameObject WinText ;
+
+    float xInput              ;
+    float zInput              ;
+    public float speed        ;
+
 
     // Start is called before the first frame update
     void Start()
@@ -21,12 +30,36 @@ public class Test1 : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Destroy(gameObject);
+            //Destroy(gameObject);
+
+            //body.AddForce(Vector3.up * 500);
+
+            //body.velocity = Vector3.forward * 20f;
         }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("Level");
+        }
+
+        xInput = Input.GetAxis("Horizontal");
+        zInput = Input.GetAxis("Vertical")  ;
+        body.AddForce(xInput * speed, 0, zInput * speed);
+
     }
 
     private void OnMouseDown()
     {
         Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter (Collision collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            //Destroy(gameObject);
+            Destroy(collision.gameObject);
+            WinText.SetActive(true)      ;
+        }
     }
 }
