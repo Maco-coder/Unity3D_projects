2675ee1f849411b861b7257890b2701a136d4fdb
Maco-coder <mamoranl@uwaterloo.ca>
2021-05-14 11:14:35 -0400

    Changes made

--- Basics/Basics_project/Assets/Scripts/GameController.cs
+++ Basics/Basics_project/Assets/Scripts/GameController.cs
@@ -8,15 +8,42 @@ public class GameController : MonoBehaviour
     public GameObject ball      ;
     public Transform spawnPoint ;
 
-    void Start() {
-        SpawnBall();
+    public float maxX;
+    public float maxZ;
+
+    void Start()
+    {
+        //SpawnBall();
+
+        InvokeRepeating("SpawnBall", 1f, 2f);
+
     }
 
     void Update() {
         
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnBall();
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = 10f                                           ;
+            Vector3 spawnPos = Camera.main.ScreenToWorldPoint(mousePos);
+            Instantiate(ball, spawnPos, Quaternion.identity)           ;
+        }
+
+
     }
 
-    void SpawnBall() {
-        Instantiate(ball, spawnPoint.position, Quaternion.identity);
+    void SpawnBall()
+    {
+        float randomX = Random.Range(-maxX, maxX)                  ;
+        float randomZ = Random.Range(-maxZ, maxZ)                  ;
+        Vector3 randomSpawnPos = new Vector3(randomX, 10f, randomZ);
+        Instantiate(ball, randomSpawnPos, Quaternion.identity)     ;
+
+        //Instantiate(ball, spawnPoint.position, Quaternion.identity);
     }
 }
