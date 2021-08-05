2ebbc0e5490308cbb19e4b3514661b910ead9c16
Maco-coder <mamoranl@uwaterloo.ca>
2021-08-04 20:12:22 -0400

    Change made

---
+++ Project#Knee/Project_knee/Assets/Scripts/Camera_Move.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Move : MonoBehaviour
+{
+
+    public float speed;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        transform.Rotate(0, speed * Time.deltaTime, 0);
+    }
+}
