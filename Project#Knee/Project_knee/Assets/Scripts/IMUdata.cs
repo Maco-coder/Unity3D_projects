12d6613035dbea636ed3c892e1a8f23a963a5d9d
Maco-coder <mamoranl@uwaterloo.ca>
2021-08-19 13:57:16 -0400

    Change made

--- Project#Knee/Project_knee/Assets/Scripts/IMUdata.cs
+++ Project#Knee/Project_knee/Assets/Scripts/IMUdata.cs
@@ -43,8 +43,8 @@ public class IMUdata : MonoBehaviour
 
             //transform.Rotate(x_value, 0, 0, Space.Self);
 
-            //Vector3 to = new Vector3(x_value, z_value, y_value);  // Raw values
-            Vector3 to = new Vector3((9 / 7) * x_value, 0, y_value - y_value);  // Mapped values
+            Vector3 to = new Vector3(x_value, 0, y_value);  // Raw values
+            //Vector3 to = new Vector3((9 / 7) * x_value, 0, y_value - y_value);  // Mapped values
             transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, to, Time.deltaTime*100);
 
             stream.BaseStream.Flush() ;
