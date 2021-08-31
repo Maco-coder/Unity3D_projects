e98e5727ee16bc6eab52f557a5449f6e7a141c07
Maco-coder <mamoranl@uwaterloo.ca>
2021-08-31 17:14:21 -0400

    Change made

--- Project#Knee/Project_knee/Assets/Scripts/IMUdata1.cs
+++ Project#Knee/Project_knee/Assets/Scripts/IMUdata1.cs
@@ -8,14 +8,17 @@ public class IMUdata1 : MonoBehaviour
 
     SerialPort stream = new SerialPort("COM4", 115200);
     public string receivedstring ;
-    //public GameObject IMU        ;
-    //public Vector3 rot1          ;
-    //public Vector3 rot2          ;
-    public string[] data         ;
-    public string[] data_received;
-    int x_value;
-    int y_value;
-    int z_value;
+    //public GameObject IMU       ;
+    //public Vector3 rot1         ;
+    //public Vector3 rot2         ;
+    public string[] data          ;
+    public string[] data_received ;
+    public int x_value1 ;
+    public int y_value1 ;
+    public int z_value1 ;
+    public int x_value2 ;
+    public int y_value2 ;
+    public int z_value2 ;
 
 
     void Start()
@@ -30,20 +33,29 @@ public class IMUdata1 : MonoBehaviour
         stream.BaseStream.Flush();
 
         string[] data = receivedstring.Split(',');
-        if (data[0] != "" && data[1] != "" && data[2] != "")
+        if (data[0] != "" && data[1] != "" && data[2] != "" && data[3] != "" && data[4] != "" && data[5] != "")
         {
             data_received[0] = data[0];
-            int.TryParse(data[0], out x_value);
+            int.TryParse(data[0], out x_value1);
 
             data_received[1] = data[1];
-            int.TryParse(data[1], out y_value);
+            int.TryParse(data[1], out y_value1);
 
             data_received[2] = data[2];
-            int.TryParse(data[2], out z_value);
+            int.TryParse(data[2], out z_value1);
+
+            data_received[3] = data[3];
+            int.TryParse(data[3], out x_value2);
+
+            data_received[4] = data[4];
+            int.TryParse(data[4], out y_value2);
+
+            data_received[5] = data[5];
+            int.TryParse(data[5], out z_value2);
 
             //transform.Rotate(x_value, 0, 0, Space.Self);
 
-            Vector3 to = new Vector3(x_value, 0, y_value);  // Raw values
+            Vector3 to = new Vector3(x_value1, 0, y_value1);  // Raw values
             //Vector3 to = new Vector3((9 / 7) * x_value, 0, y_value - y_value);  // Mapped values
             transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, to, Time.deltaTime*100);
 
--- Project#Knee/Project_knee/Assets/Scripts/IMUdata2.cs
+++ Project#Knee/Project_knee/Assets/Scripts/IMUdata2.cs
@@ -6,60 +6,63 @@ using System.IO.Ports            ;
 public class IMUdata2 : MonoBehaviour
 {
 
-    SerialPort stream = new SerialPort("COM4", 115200);
-    public string receivedstring ;
+    //SerialPort stream = new SerialPort("COM4", 115200);
+    //public string receivedstring  ;
     //public GameObject IMU       ;
     //public Vector3 rot1         ;
     //public Vector3 rot2         ;
-    public string[] data          ;
-    public string[] data_received ;
-    int x_value1;
-    int y_value1;
-    int z_value1;
-    int x_value2;
-    int y_value2;
-    int z_value2;
+    //public string[] data          ;
+    //public string[] data_received ;
+    //int x_value1;
+    //int y_value1;
+    //int z_value1;
+    //int x_value2;
+    //int y_value2;
+    //int z_value2;
 
 
     void Start()
     {
-        stream.Open();
+        //stream.Open();
+
     }
 
     
     void Update()
     {
-        receivedstring = stream.ReadLine() ;
-        stream.BaseStream.Flush()          ;
+        //receivedstring = stream.ReadLine() ;
+        //stream.BaseStream.Flush()          ;
 
-        string[] data = receivedstring.Split(',');
-        if (data[0] != "" && data[1] != "" && data[2] != "" && data[3] != "" && data[4] != "" && data[5] != "")
-        {
-            data_received[0] = data[0];
-            int.TryParse(data[0], out x_value1);
+        //string[] data = receivedstring.Split(',');
+        //if (data[0] != "" && data[1] != "" && data[2] != "" && data[3] != "" && data[4] != "" && data[5] != "")
+        //{
+        //    data_received[0] = data[0];
+        //    int.TryParse(data[0], out x_value1);
+        //
+        //    data_received[1] = data[1];
+        //    int.TryParse(data[1], out y_value1);
 
-            data_received[1] = data[1];
-            int.TryParse(data[1], out y_value1);
+        //    data_received[2] = data[2];
+        //    int.TryParse(data[2], out z_value1);
 
-            data_received[2] = data[2];
-            int.TryParse(data[2], out z_value1);
+        //    data_received[3] = data[3];
+        //    int.TryParse(data[3], out x_value2);
 
-            data_received[3] = data[3];
-            int.TryParse(data[3], out x_value2);
+        //    data_received[4] = data[4];
+        //    int.TryParse(data[4], out y_value2);
 
-            data_received[4] = data[4];
-            int.TryParse(data[4], out y_value2);
-
-            data_received[5] = data[5];
-            int.TryParse(data[5], out z_value2);
+        //    data_received[5] = data[5];
+        //    int.TryParse(data[5], out z_value2);
 
             //transform.Rotate(x_value, 0, 0, Space.Self);
 
-            //Vector3 to = new Vector3(x_value1, 0, y_value1);  // Raw values
+            Vector3 to = new Vector3(x, 0, y);  // Raw values
             //Vector3 to = new Vector3((9 / 7) * x_value1, 0, y_value1 - y_value1);  // Mapped values
             //transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, to, Time.deltaTime*100);
 
-            stream.BaseStream.Flush() ;
-        }
+        //   stream.BaseStream.Flush() ;
+
+
+        //}
     }
 }
