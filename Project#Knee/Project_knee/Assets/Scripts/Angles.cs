a83a3e044f3d230b20a908341188d479d2860fcc
Maco-coder <mamoranl@uwaterloo.ca>
2021-09-14 12:27:39 -0400

    Change made

---
+++ Project#Knee/Project_knee/Assets/Scripts/Angles.cs
@@ -0,0 +1,26 @@
+//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Angles : MonoBehaviour
+{
+
+    public Transform element ;
+    public Text x_text      ;
+    public Text y_text      ;
+    public Text z_text      ;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //Debug.Log(element.rotation.z);
+        x_text.text = element.position.x.ToString();
+
+    }
+}
