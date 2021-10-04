343239710107256276a53f4d35ec8142fb0f26fe
Maco-coder <mamoranl@uwaterloo.ca>
2021-10-04 18:57:16 -0400

    Change made

--- Project#Knee/Project_knee/Assets/Scripts/IMUdata1.cs
+++ Project#Knee/Project_knee/Assets/Scripts/IMUdata1.cs
@@ -30,6 +30,10 @@ public class IMUdata1 : MonoBehaviour
         stream.BaseStream.Flush();
 
         string[] data = receivedstring.Split(',');
+
+
+        // DATA FROM TWO ACCELEROMETERS //
+
         //if (data[0] != "" && data[1] != "" && data[2] != "" && data[3] != "" && data[4] != "" && data[5] != "")
         //{
         //    data_received[0] = data[0];
