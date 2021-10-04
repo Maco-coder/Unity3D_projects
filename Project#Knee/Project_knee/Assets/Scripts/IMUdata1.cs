6a5d10d9b78af9d1c6d276b37a638437cd0f46ca
Maco-coder <mamoranl@uwaterloo.ca>
2021-10-04 19:10:08 -0400

    Change made

--- Project#Knee/Project_knee/Assets/Scripts/IMUdata1.cs
+++ Project#Knee/Project_knee/Assets/Scripts/IMUdata1.cs
@@ -69,25 +69,35 @@ public class IMUdata1 : MonoBehaviour
         // FOR TESTING PURPOSES WITH ONLY ONE ACCELEROMETER //
 
 
+        //if (data[0] != "" && data[1] != "" && data[2] != "")
+        //{
+        //    data_received[0] = data[0];
+        //    int.TryParse(data[0], out x_value1);
+
+        //    data_received[1] = data[1];
+        //    int.TryParse(data[1], out y_value1);
+
+        //    data_received[2] = data[2];
+        //    int.TryParse(data[2], out z_value1);
+
+        //    //transform.Rotate(x_value, 0, 0, Space.Self);
+
+        //    //Vector3 to = new Vector3(x_value1, 0, y_value1);  // Raw values
+        //    Vector3 to = new Vector3((9 / 7) * x_value1, 0, y_value1 - y_value1);  // Mapped values
+        //    transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, to, Time.deltaTime * 100);
+
+
+        // DATA FROM THE ACCELEROMETER_MATH ARDUINO CODE //
         if (data[0] != "" && data[1] != "" && data[2] != "")
         {
             data_received[0] = data[0];
             int.TryParse(data[0], out x_value1);
+            Vector3 to = new Vector3(x_value1, 0, 0);
+        }
 
-            data_received[1] = data[1];
-            int.TryParse(data[1], out y_value1);
 
-            data_received[2] = data[2];
-            int.TryParse(data[2], out z_value1);
 
-            //transform.Rotate(x_value, 0, 0, Space.Self);
-
-            //Vector3 to = new Vector3(x_value1, 0, y_value1);  // Raw values
-            Vector3 to = new Vector3((9 / 7) * x_value1, 0, y_value1 - y_value1);  // Mapped values
-            transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, to, Time.deltaTime * 100);
-            
-
-            stream.BaseStream.Flush();
+        stream.BaseStream.Flush();
         }
 
 
