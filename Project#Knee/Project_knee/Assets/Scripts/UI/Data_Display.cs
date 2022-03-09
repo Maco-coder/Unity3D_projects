1998cc03b426269517e0121f22b4d06cb70e2b30
Maco-coder <mamoranl@uwaterloo.ca>
2022-03-09 01:01:03 -0500

    Change made

--- Project#Knee/Project_knee/Assets/Scripts/UI/Data_Display.cs
+++ Project#Knee/Project_knee/Assets/Scripts/UI/Data_Display.cs
@@ -15,7 +15,8 @@ public class Data_Display : MonoBehaviour
     public Text angle_abduction_adduction ;
     public Text angle_external_internal   ;
     public Text angle_servo_position      ;
-    public Text force_FSR                 ;
+    public Text force_FSR1                ;
+    public Text force_FSR2                ;
 
 
     void Start()
@@ -29,7 +30,8 @@ public class Data_Display : MonoBehaviour
         angle_flexion_extension.text = cube_IMU.eulerAngles.x.ToString("0")   ;
         angle_abduction_adduction.text = cube_IMU.eulerAngles.z.ToString("0") ;
         angle_external_internal.text = cube_IMU.eulerAngles.y.ToString("0")   ;
-        force_FSR.text = cube_FSR.eulerAngles.x.ToString("0")                 ;
+        force_FSR1.text = cube_FSR.eulerAngles.x.ToString("0")                ;
+        force_FSR2.text = cube_FSR.eulerAngles.x.ToString("0")                ;
         angle_servo_position.text = cube_Servo.eulerAngles.x.ToString("0")    ;
 
     }
--- Project#Knee/Project_knee/Assets/Scripts/UI/Sensors.cs
+++ Project#Knee/Project_knee/Assets/Scripts/UI/Sensors.cs
@@ -16,8 +16,8 @@ public class Sensors : MonoBehaviour
 
     // GameObjects //
     public Transform gameObject1 ;  // Cube_IMU
-    public Transform gameObject2 ;  // Cube_FSR
-    public Transform gameObject3 ;  // Cube_Strain
+    public Transform gameObject2 ;  // Cube_FSR1
+    public Transform gameObject3 ;  // Cube_FSR2
     public Transform gameObject4 ;  // Cube_Servo
 
     // Knee motion data //
@@ -27,10 +27,10 @@ public class Sensors : MonoBehaviour
 
     // Tension/Injury data //
     public int Servo_pos_value;
-    public int Strain_value   ;
 
     // Force data //
-    public int FSR_value;
+    public int FSR1_value;
+    public int FSR2_value;
 
     // Displaying data //
     //public Transform cube_IMU;
@@ -40,7 +40,8 @@ public class Sensors : MonoBehaviour
     public Text angle_abduction_adduction;
     public Text angle_external_internal;
     public Text angle_servo_position;
-    public Text force_FSR;
+    public Text force_FSR1;
+    public Text force_FSR2;
 
 
     void Start()
@@ -62,53 +63,54 @@ public class Sensors : MonoBehaviour
         int.TryParse(data[0], out Servo_pos_value);
 
         data_received[1] = data[1];
-        int.TryParse(data[1], out FSR_value);
+        int.TryParse(data[1], out FSR1_value);
 
         data_received[2] = data[2];
-        int.TryParse(data[2], out Strain_value);
+        int.TryParse(data[2], out FSR2_value);
 
-        data_received[3] = data[3];
-        int.TryParse(data[3], out Gyr_X_value);
+        //data_received[3] = data[3];
+        //int.TryParse(data[3], out Gyr_X_value);
 
-        data_received[4] = data[4];
-        int.TryParse(data[4], out Gyr_Y_value);
+        //data_received[4] = data[4];
+        //int.TryParse(data[4], out Gyr_Y_value);
 
-        data_received[5] = data[5];
-        int.TryParse(data[5], out Gyr_Z_value);
+        //data_received[5] = data[5];
+        //int.TryParse(data[5], out Gyr_Z_value);
 
-        Vector3 to1 = new Vector3(Gyr_X_value, Gyr_Z_value, Gyr_Y_value);
-        Vector3 to2 = new Vector3(FSR_value, 0, 0)                      ;
-        Vector3 to4 = new Vector3(Servo_pos_value, 0, 0)                ;
+        //Vector3 to1 = new Vector3(Gyr_X_value, Gyr_Z_value, Gyr_Y_value);
+        //Vector3 to2 = new Vector3(FSR_value, 0, 0)                      ;
+        //Vector3 to4 = new Vector3(Servo_pos_value, 0, 0)                ;
 
-        gameObject1.transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, to1, Time.deltaTime * 100);
-        gameObject2.transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, to2, Time.deltaTime * 100);
+        //gameObject1.transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, to1, Time.deltaTime * 100);
+        //gameObject2.transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, to2, Time.deltaTime * 100);
         //gameObject3.transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, to3, Time.deltaTime * 100);
-        gameObject4.transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, to4, Time.deltaTime * 100);
+        //gameObject4.transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, to4, Time.deltaTime * 100);
 
-        angle_flexion_extension.text = Gyr_X_value.ToString("0")  ;
-        angle_abduction_adduction.text = Gyr_Y_value.ToString("0");
-        angle_external_internal.text = Gyr_Z_value.ToString("0")  ;
-        angle_servo_position.text = Servo_pos_value.ToString("0") ;
+        //angle_flexion_extension.text = Gyr_X_value.ToString("0")  ;
+        //angle_abduction_adduction.text = Gyr_Y_value.ToString("0");
+        //angle_external_internal.text = Gyr_Z_value.ToString("0")  ;
+        //angle_servo_position.text = Servo_pos_value.ToString("0") ;
         //force_FSR.text = FSR_value.ToString("0")                ;
 
-        if (FSR_value >= 0 && FSR_value < 500)
+
+        if (FSR1_value >= 0 && FSR1_value < 500)
         {
-            force_FSR.text = ("High pressure") ;
+            force_FSR1.text = ("High pressure") ;
         }
 
-        if (FSR_value >= 500 && FSR_value < 750)
+        if (FSR1_value >= 500 && FSR1_value < 750)
         {
-            force_FSR.text = ("Medium pressure");
+            force_FSR1.text = ("Medium pressure");
         }
 
-        if (FSR_value >= 750 && FSR_value < 1000)
+        if (FSR1_value >= 750 && FSR1_value < 1000)
         {
-            force_FSR.text = ("Low pressure");
+            force_FSR1.text = ("Low pressure");
         }
 
-        if (FSR_value >= 1000)
+        if (FSR1_value >= 1000)
         {
-            force_FSR.text = ("No pressure");
+            force_FSR1.text = ("No pressure");
         }
 
     }
