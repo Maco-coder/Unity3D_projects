421f5768526dde332cd67ddbeffd2b3d3fcaa342
Maco-coder <mamoranl@uwaterloo.ca>
2022-08-10 19:37:32 -0400

    change made

---
+++ Project_Responsive Design Haptics/Responsive_Design_Haptics_v1/Assets/Vibration_Pluck.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vibration_Pluck : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
