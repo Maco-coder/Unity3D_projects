5e56d8543c5a9c9adb2428497066188d2b957ef3
Maco-coder <mamoranl@uwaterloo.ca>
2022-08-11 01:36:31 -0400

    change made

---
+++ Project_Responsive Design Haptics/Responsive_Design_Haptics_v1/Assets/Haptics.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections        ;
+using System.Collections.Generic;
+using UnityEngine               ;
+using Valve.VR                  ;
+
+
+public class Haptics : MonoBehaviour
+{
+    public SteamVR_Action_Vibration hapticAction;
+    public SteamVR_Action_Boolean trackpadAction;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
