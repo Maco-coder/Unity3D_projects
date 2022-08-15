59c3ae72c1aa8084f4415f3d0fee4ab0862a59f5
Maco-coder <mamoranl@uwaterloo.ca>
2022-08-15 18:03:54 -0400

    change made

---
+++ Project_Responsive Design Haptics/Responsive_Design_Haptics_v1/Assets/Scripts/Set_Object_Active.cs
@@ -0,0 +1,44 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Set_Object_Active : MonoBehaviour
+{
+
+    //public GameObject Joint_Object1;
+    public float speed = 2.5f;
+
+
+    void Start()
+    {
+        //Joint_Object1.SetActive(true);
+    }
+
+
+    void Update()
+    {
+
+        transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.PingPong(Time.time * speed, 5));
+        //if (Input.GetKeyDown(KeyCode.X))
+        //{
+        //    Joint_Object1.SetActive(true);
+        //}
+
+        //if (Input.GetKeyDown(KeyCode.Z))
+        //{
+            //Joint_Object1.SetActive(false);
+        //}
+    }
+
+
+    void onCollisionEnter (Collision col)
+    {
+        if (col.gameObject.name == "Cube2")
+        {
+            Debug.Log("Collision Detected");
+            Destroy(col.gameObject)        ;
+        }
+    }
+
+}
