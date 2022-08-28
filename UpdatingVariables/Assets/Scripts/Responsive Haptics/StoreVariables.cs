a9827fc33c72fc700a6645796b1bb0fed80b45b9
Maco-coder <mamoranl@uwaterloo.ca>
2022-08-27 20:46:07 -0400

    change made

--- UpdatingVariables/Assets/Scripts/Responsive Haptics/StoreVariables.cs
+++ UpdatingVariables/Assets/Scripts/Responsive Haptics/StoreVariables.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class StoreVariables : MonoBehaviour
 {
+	
+	// VIVE controller parameters //
+
 	public static float VRcontroller_GRAB_pulse_time     ;
 	public static float VRcontroller_GRAB_pulse_frequency;
 	public static float VRcontroller_GRAB_pulse_amplitude;
+
+	public static float VRcontroller_PLUCK_pulse_time     ;
+	public static float VRcontroller_PLUCK_pulse_frequency;
+	public static float VRcontroller_PLUCK_pulse_amplitude;
+
+	public static float VRcontroller_SPRING_pulse_time     ;
+	public static float VRcontroller_SPRING_pulse_frequency;
+	public static float VRcontroller_SPRING_pulse_amplitude;
+
+
+	// 3D Touch System parameters //
 
 	public static float Touch_vibration_gain     ;
 	public static float Touch_vibration_magnitude;
