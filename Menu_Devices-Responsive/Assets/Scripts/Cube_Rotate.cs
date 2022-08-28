acd68152f30516ff645fe849fa0f4d6eafec7ca4
Maco-coder <mamoranl@uwaterloo.ca>
2022-08-28 01:10:11 -0400

    change made

---
+++ Menu_Devices-Responsive/Assets/Scripts/Cube_Rotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube_Rotate : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        transform.Rotate(Vector3.right * StoreVariables.VRcontroller_GRAB_pulse_time * Time.deltaTime)       ;
+        transform.Rotate(Vector3.up * StoreVariables.VRcontroller_GRAB_pulse_frequency * Time.deltaTime)     ;
+        transform.Rotate(Vector3.forward * StoreVariables.VRcontroller_GRAB_pulse_amplitude * Time.deltaTime);
+    }
+}
