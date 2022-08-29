f5a2496bba915a25a5f42426c64dd499b268a03f
Maco-coder <mamoranl@uwaterloo.ca>
2022-08-29 11:34:20 -0400

    Change made

--- Menu_Devices-Responsive/Assets/Scripts/GoToExperiences.cs
+++ Menu_Devices-Responsive/Assets/Scripts/GoToExperiences.cs
@@ -1,18 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.Collections         ;
+using System.Collections.Generic ;
+using UnityEngine                ;
+using UnityEngine.UI             ;
+using UnityEngine.SceneManagement;
 
 public class GoToExperiences : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public void GoToMenuExperiences()
+	{
+		SceneManager.LoadScene("MainMenu_Experiences");
+	}
 }
