eae7e4791c09e7f8b1b9fb37a7e6c851198af4c2
Maco-coder <mamoranl@uwaterloo.ca>
2022-08-29 14:58:13 -0400

    change made

---
+++ Project_Responsive Design Haptics/Responsive_Design_Haptics_v1/Assets/Scripts/MenuScripts/KeysPressed.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeysPressed : MonoBehaviour
+{
+
+    public GameObject playerVR;
+    
+    void Start()
+    {
+        playerVR.SetActive (false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space")){
+            playerVR.SetActive(true);
+        }
+    }
+}
