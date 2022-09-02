4349a600b5c17df5dce4de68d815bc5b7e8a129d
Maco-coder <mamoranl@uwaterloo.ca>
2022-09-02 13:19:06 -0400

    change made

--- JSON_Unity_Testing/Assets/Scripts/Cube1Data.cs
+++ JSON_Unity_Testing/Assets/Scripts/Cube1Data.cs
@@ -8,27 +8,43 @@ public class Cube1Data : MonoBehaviour
 {
 
     public GameObject Cube1 ;
-    
-    Vector3 m_NewSpeed;
 
-    private float Pos_X = 0 ;
-    private float Pos_Y = 1 ;
-    private float Pos_Z = 0 ;
+    //private float Pos_X = 0 ;
+    //private float Pos_Y = 1 ;
+    //private float Pos_Z = 0 ;
 
-    private float Scale_X = 1 ;
-    private float Scale_Y = 1 ;
-    private float Scale_Z = 1 ;
+    //private float Scale_X = 1 ;
+    //private float Scale_Y = 1 ;
+    //private float Scale_Z = 1 ;
 
-    private float speed_X = 0 ;
-    private float speed_Y = 0 ;
-    private float speed_Z = 0 ;
+    //private float speed_X = 0 ;
+    //private float speed_Y = 0 ;
+    //private float speed_Z = 0 ;
 
-    private string EnableGravity = "no";
+    //private string EnableGravity = "no";
+
+    Vector3 m_position;
+    Vector3 m_scale   ;
+    Vector3 m_speed   ;
+    string m_gravity  ;
 
 
     void Start()
     {
-        Debug.Log("GameHandler.start")  ;
+
+        //Cube1.transform.position = new Vector3(Pos_X, Pos_Y, Pos_Z)         ;  // Position setting //
+        //Cube1.transform.localScale = new Vector3(Scale_X, Scale_Y, Scale_Z) ;  // Scale setting    //
+
+        //if (EnableGravity == "yes"){
+        //    Cube1.GetComponent<Rigidbody>().useGravity = true ;  // Gravity enabling //
+        //}
+
+        //if (EnableGravity == "no"){
+        //    Cube1.GetComponent<Rigidbody>().useGravity = false ;  // Gravity enabling //
+        //}
+
+
+        Debug.Log("GameHandler.start") ;
         string json = File.ReadAllText(Application.dataPath + "/CubeExperience.json");
         PlayerData loadedPlayerData = JsonUtility.FromJson<PlayerData>(json)         ;
         Debug.Log("position: "+loadedPlayerData.position);
@@ -36,28 +52,29 @@ public class Cube1Data : MonoBehaviour
         Debug.Log("speed: "+loadedPlayerData.speed)      ;
         Debug.Log("gravity: "+loadedPlayerData.gravity)  ;
 
-        
-        Cube1.transform.position = new Vector3(Pos_X, Pos_Y, Pos_Z)         ;  // Position setting //
-        Cube1.transform.localScale = new Vector3(Scale_X, Scale_Y, Scale_Z) ;  // Scale setting    //
+        m_position = new Vector3(loadedPlayerData.position.x, loadedPlayerData.position.y, loadedPlayerData.position.z);
+        m_scale = new Vector3(loadedPlayerData.scale.x, loadedPlayerData.scale.y, loadedPlayerData.scale.z)            ;
+        m_speed = new Vector3(loadedPlayerData.speed.x, loadedPlayerData.speed.y, loadedPlayerData.speed.z)            ;
+        m_gravity = loadedPlayerData.gravity;
 
-        if (EnableGravity == "yes"){
+        Cube1.transform.position = new Vector3(m_position.x, m_position.y, m_position.z) ;
+        Cube1.transform.localScale = new Vector3(m_scale.x, m_scale.y, m_scale.z)        ;
+
+
+        if (m_gravity == "yes"){
             Cube1.GetComponent<Rigidbody>().useGravity = true ;  // Gravity enabling //
         }
 
-        if (EnableGravity == "no"){
+        if (m_gravity == "no" || m_gravity == null){
             Cube1.GetComponent<Rigidbody>().useGravity = false ;  // Gravity enabling //
         }
-
-
-        m_NewSpeed = new Vector3(loadedPlayerData.speed.x, loadedPlayerData.speed.y, loadedPlayerData.speed.z);
-
 
     }
 
     
     void Update()
     {
-        Cube1.transform.Rotate(m_NewSpeed.x, m_NewSpeed.y, m_NewSpeed.z, Space.World);  // Speed setting //
+        Cube1.transform.Rotate(m_speed.x, m_speed.y, m_speed.z, Space.World);  // Speed setting //
     }
 
     
