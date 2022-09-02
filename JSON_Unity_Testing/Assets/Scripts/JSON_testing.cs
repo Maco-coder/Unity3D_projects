99904c2574cdb5abe5d602eaea9e54166e941c91
Maco-coder <mamoranl@uwaterloo.ca>
2022-09-02 10:12:19 -0400

    change made

--- JSON_Unity_Testing/Assets/Scripts/JSON_testing.cs
+++ JSON_Unity_Testing/Assets/Scripts/JSON_testing.cs
@@ -14,24 +14,24 @@ public class JSON_testing : MonoBehaviour
         /*
         PlayerData playerData = new PlayerData();
         playerData.position = new Vector3(5,0);
-        playerData.health = 80;
+        playerData.acceleration = 80;
 
         string json = JsonUtility.ToJson(playerData);
         Debug.Log(json);
 
-        File.WriteAllText(Application.dataPath + "saveFile.json", json);
+        File.WriteAllText(Application.dataPath + "FileJson.json", json);
         */
 
-        string json = File.ReadAllText(Application.dataPath + "/saveFile.json");
-        PlayerData loadedPlayerData = JsonUtility.FromJson<PlayerData>(json);
-        Debug.Log("position: "+loadedPlayerData.position);
-        Debug.Log("health: "+loadedPlayerData.health)    ;
+        string json = File.ReadAllText(Application.dataPath + "/sample_v1.json");
+        PlayerData loadedPlayerData = JsonUtility.FromJson<PlayerData>(json)   ;
+        Debug.Log("position: "+loadedPlayerData.position)        ;
+        Debug.Log("acceleration: "+loadedPlayerData.acceleration);
     }
 
     private class PlayerData
     {
         public Vector3 position;
-        public int health      ;
+        public int acceleration;
     }
 
 }
