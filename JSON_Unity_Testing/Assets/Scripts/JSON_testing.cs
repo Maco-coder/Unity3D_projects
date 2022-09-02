03863f4e665bcc75cab8bf84de7af6e9c89e7deb
Maco-coder <mamoranl@uwaterloo.ca>
2022-09-01 20:40:20 -0400

    change made

--- JSON_Unity_Testing/Assets/Scripts/JSON_testing.cs
+++ JSON_Unity_Testing/Assets/Scripts/JSON_testing.cs
@@ -1,18 +1,35 @@
-﻿using System.Collections;
+﻿using System.IO                 ;
+using System.Collections        ;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine               ;
+
 
 public class JSON_testing : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    private void Start()
     {
-        
+        Debug.Log("GameHandler.start")  ;
+
+        PlayerData playerData = new PlayerData();
+        playerData.position = new Vector3(5,0);
+        playerData.health = 80;
+
+        string json = JsonUtility.ToJson(playerData);
+        Debug.Log(json);
+
+        File.WriteAllText(Application.dataPath + "saveFile.json", json);
+
+        //string json = File.ReadAllText(Application.dataPath + "/saveFile.json");
+        //PlayerData loadedPlayerData = JsonUtility.FromJson<PlayerData>(json);
+        //Debug.Log("position: "+loadedPlayerData.position);
+        //Debug.Log("health: "+loadedPlayerData.health)    ;
     }
 
-    // Update is called once per frame
-    void Update()
+    private class PlayerData
     {
-        
+        public Vector3 position;
+        public int health      ;
     }
+
 }
