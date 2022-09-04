dd5fcbaf85a658ca513c11fd6d28de58c661564e
Maco-coder <mamoranl@uwaterloo.ca>
2022-09-04 19:12:45 -0400

    change made

--- JSON_Unity_Testing/Assets/Scripts/Haptics.cs
+++ JSON_Unity_Testing/Assets/Scripts/Haptics.cs
@@ -6,8 +6,38 @@ using Valve.VR;
 
 public class Haptics : MonoBehaviour
 {
-    public SteamVR_Action_Single squeezeAction;
+
+    public GameObject Cube2  ;
+    public TextAsset textJSON;
+
+    [System.Serializable]
+    public class Data
+    {
+        public string type     ;
+        public string magnitude;
+        public string geometry ;
+    }
+
+    [System.Serializable]
+    public class List
+    {
+        public Data[] feedback ;
+
+    }
+
+    public List myFeedbackList = new List();
+
+    public SteamVR_Action_Single squeezeAction  ;
     public SteamVR_Action_Vibration hapticAction;
+
+
+    void Start()
+    {
+        myFeedbackList = JsonUtility.FromJson<List>(textJSON.text);
+        print(myFeedbackList.feedback[0].type);
+
+    }
+
 
     void Update()
     {
