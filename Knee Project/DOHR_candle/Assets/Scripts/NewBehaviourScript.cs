7363ba72991d928a941251e3001df6b7d2fe35ff
Maco-coder <mamoranl@uwaterloo.ca>
2023-03-23 12:21:35 -0400

    change made

--- Knee Project/DOHR_candle/Assets/Scripts/NewBehaviourScript.cs
+++ Knee Project/DOHR_candle/Assets/Scripts/NewBehaviourScript.cs
@@ -1,16 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System.Collections         ;
+using System.Collections.Generic ;
+using UnityEngine                ;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject tracked_object ;
+    public GameObject candle         ;
+
     void Start()
     {
-
+        candle.transform.position = new Vector3(tracked_object.transform.position.x, tracked_object.transform.position.y + 0.50f, tracked_object.transform.position.z);
     }
 
-    // Update is called once per frame
     void Update()
     {
         
