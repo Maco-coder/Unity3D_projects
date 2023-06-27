12ba2af44a02e4ac59f8962bffd9fd485cf8ea39
Maco-coder <mamoranl@uwaterloo.ca>
2023-06-27 11:25:29 -0400

    change made

---
+++ Responsive_Haptics_S2023/Unity_JSON_Responsive_Haptics_S2023/Assets/Scripts/Read_JSON_Data.cs
@@ -0,0 +1,19 @@
+using System.Collections        ;
+using System.Collections.Generic;
+using UnityEngine               ;
+using System.IO                 ;
+
+public class Read_JSON_Data : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
