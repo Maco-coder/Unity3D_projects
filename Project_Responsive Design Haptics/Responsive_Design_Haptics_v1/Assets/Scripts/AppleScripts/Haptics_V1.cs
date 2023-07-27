39e895934908d82122e13a0b9087fc20aa06fe4a
Maco-coder <mamoranl@uwaterloo.ca>
2023-07-27 13:59:06 -0400

    change made

--- Project_Responsive Design Haptics/Responsive_Design_Haptics_v1/Assets/Scripts/AppleScripts/Haptics_V1.cs
+++ Project_Responsive Design Haptics/Responsive_Design_Haptics_v1/Assets/Scripts/AppleScripts/Haptics_V1.cs
@@ -10,12 +10,17 @@ public class Haptics_V1 : MonoBehaviour
     public SteamVR_Action_Single squeezeAction   ;
     public SteamVR_Action_Vibration hapticAction ;
 
+    bool LisCurrentlyColliding;
+    bool RisCurrentlyColliding;
+
+
     void Update()
     {
 
         float triggerValueL = squeezeAction.GetAxis(SteamVR_Input_Sources.LeftHand);
 
-        if (triggerValueL > 0.0f)
+        if ( (triggerValueL > 0.0f) && LisCurrentlyColliding)
+        //if (triggerValueL > 0.0f)
         {
             print(triggerValueL);
             //Pulse(StoreVariables.VRcontroller_GRAB_pulse_time, StoreVariables.VRcontroller_GRAB_pulse_frequency, StoreVariables.VRcontroller_GRAB_pulse_amplitude, SteamVR_Input_Sources.LeftHand) ;
@@ -25,11 +30,11 @@ public class Haptics_V1 : MonoBehaviour
 
         float triggerValueR = squeezeAction.GetAxis(SteamVR_Input_Sources.RightHand);
 
-        if (triggerValueR > 0.0f)
+        if ( (triggerValueR > 0.0f) && RisCurrentlyColliding )
         {
             print(triggerValueR);
             //Pulse(StoreVariables.VRcontroller_GRAB_pulse_time, StoreVariables.VRcontroller_GRAB_pulse_frequency, StoreVariables.VRcontroller_GRAB_pulse_amplitude, SteamVR_Input_Sources.RightHand) ;
-            Pulse(1, 150, 75, SteamVR_Input_Sources.LeftHand);
+            Pulse(1, 150, 75, SteamVR_Input_Sources.RightHand);
         }
 
     }
@@ -38,6 +43,29 @@ public class Haptics_V1 : MonoBehaviour
     {
         hapticAction.Execute(0, duration, frequency, amplitude, source);
         print("Pulse" + "" + source.ToString());
+    }
+
+
+    void OnCollisionEnter(Collision collision){
+        
+        if (collision.gameObject.name == "LHand"){
+            Debug.Log("Left hand touching apple") ;
+            LisCurrentlyColliding = true          ;
+        }
+
+        if (collision.gameObject.name == "RHand"){
+            Debug.Log("Right hand touching apple") ;
+            RisCurrentlyColliding = true           ;
+        }
+        
+    }
+
+
+    void OnCollisionExit(Collision collision){
+
+        LisCurrentlyColliding = false ;
+        RisCurrentlyColliding = false ;
+        
     }
 
 }
