2ffc81886099e9a8564b51df2139162cb1b036fd
Maco-coder <mamoranl@uwaterloo.ca>
2023-08-13 16:21:47 -0400

    change made

---
+++ Project_Responsive Design Haptics/Responsive_Design_Haptics_universal/Assets/Scripts/OpeningScene_Devices.cs
@@ -0,0 +1,50 @@
+
+using System.Collections          ;
+using System.Collections.Generic  ;
+using UnityEngine                 ;
+using UnityEngine.UI              ;
+using UnityEngine.SceneManagement ;
+
+
+public class OpeningScene_Devices : MonoBehaviour
+{
+
+    public Button ButtonVRController;
+    public Button Button3DTouch     ;
+    public Button ButtonFruitPicker ;
+
+    private int chosen_device = 0 ;
+
+
+    public void DeviceChosenIsVRController ()
+    {
+        chosen_device = 1                        ;
+        SceneManager.LoadScene("UNIVERSAL_Scene");
+    }
+
+
+    public void DeviceChosenIs3DTouch ()
+    {
+        chosen_device = 2                        ;
+        SceneManager.LoadScene("UNIVERSAL_Scene");
+    }
+
+    public void DeviceChosenIsFruitPicker ()
+    {
+        chosen_device = 3                        ;
+        SceneManager.LoadScene("UNIVERSAL_Scene");
+    }
+
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+          
+    }
+
+}
