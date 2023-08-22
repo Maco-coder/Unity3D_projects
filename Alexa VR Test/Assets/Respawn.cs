8a9c381be15626eae2457b501e00f2350bfb3cd4
alexalotl <32069762+alexalotl@users.noreply.github.com>
2023-08-22 14:11:46 -0400

    minor changes

--- Alexa VR Test/Assets/Respawn.cs
+++ Alexa VR Test/Assets/Respawn.cs
@@ -13,16 +13,13 @@ public class Respawn : MonoBehaviour
         origin_pos = transform.position;
     }
 
-    void Update()
-    {
-        transform.position += new Vector3(0.01f, 0.0f, 0.0f);
-        GetComponent<Rigidbody>().velocity = Vector3.zero;
-    }
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject == bounding_area)
         {
+            print("RESPAWNED " + gameObject.name);
             transform.position = origin_pos;
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
         }
     }
 }
