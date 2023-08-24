3424fe3de1a1c69d5681f97ab75f403a98118e8c
Maco-coder <mamoranl@uwaterloo.ca>
2023-08-24 19:40:32 -0400

    change made

--- Project_Responsive Design Haptics/Responsive_Design_Haptics_universal/Assets/Scripts/JSON/ToolManager.cs
+++ Project_Responsive Design Haptics/Responsive_Design_Haptics_universal/Assets/Scripts/JSON/ToolManager.cs
@@ -30,7 +30,7 @@ public class ToolManager : MonoBehaviour
             // string params_JSON = (string) FileData[i]["params"];
             List<string> params_JSON = new List<string>();
             for (int j = 0; j < count_devices; j++) {
-                string param = (string) FileData[i]["params"][j];
+                string param = JsonMapper.ToJson(FileData[i]["params"][j]);
                 params_JSON.Add(param);
             }
             // This is for tool-related JSON data
