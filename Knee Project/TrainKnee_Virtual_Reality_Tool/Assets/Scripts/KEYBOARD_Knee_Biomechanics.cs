13dc6e1a31e04692b920cd1e030e44992c6ecaa5
Maco-coder <mamoranl@uwaterloo.ca>
2024-07-01 12:54:50 -0400

    Change made

--- Knee Project/TrainKnee_Virtual_Reality_Tool/Assets/Scripts/KEYBOARD_Knee_Biomechanics.cs
+++ Knee Project/TrainKnee_Virtual_Reality_Tool/Assets/Scripts/KEYBOARD_Knee_Biomechanics.cs
@@ -3,7 +3,7 @@ using System.Collections        ;
 using System.Collections.Generic;
 using UnityEngine               ;
 
-public class Knee_Biomechanics : MonoBehaviour
+public class KEYBOARD_Knee_Biomechanics : MonoBehaviour
 {
     
     public int selector      ;
---
+++ Knee Project/TrainKnee_Virtual_Reality_Tool/Assets/Scripts/MPU_Knee_Biomechanics.cs
@@ -0,0 +1,46 @@
+
+using System.Collections         ;
+using System.Collections.Generic ;
+using UnityEngine                ;
+using UnityEngine.UI             ;
+using System.IO.Ports            ;
+
+public class MPU_Knee_Biomechanics : MonoBehaviour
+{
+    
+    SerialPort stream = new SerialPort("COM4", 115200) ;
+
+    public string receivedstring  ;
+    public string[] data          ;
+    public string[] data_received ;
+
+    public GameObject Cube;
+
+    public int x_value ;
+    public int y_value ;
+    public int z_value ;
+
+
+    void Start()
+    {
+        stream.Open();
+    }
+
+    
+    void Update()
+    {
+        receivedstring = stream.ReadLine();
+        stream.BaseStream.Flush()         ;
+
+        string[] data = receivedstring.Split(',');
+
+        data_received[0] = data[0];
+        int.TryParse(data[0], out x_value);
+
+        data_received[1] = data[1];
+        int.TryParse(data[1], out y_value);
+
+        data_received[2] = data[2];
+        int.TryParse(data[2], out z_value);
+    }
+}
