87e35701a016195092cca97f410932abba9c4a2f
Maco-coder <mamoranl@uwaterloo.ca>
2024-07-30 21:46:02 -0400

    Change made

--- Knee Project/TrainKnee_Virtual_Reality_Tool/Assets/Scripts/ALL_MPU_Knee_Biomechanics.cs
+++ Knee Project/TrainKnee_Virtual_Reality_Tool/Assets/Scripts/ALL_MPU_Knee_Biomechanics.cs
@@ -14,16 +14,29 @@ public class ALL_MPU_Knee_Biomechanics : MonoBehaviour
     public string[] data          ;
     public string[] data_received ;
 
-    public GameObject Cube;
-    public GameObject Leg ;
+    public GameObject Cube ;
+    public GameObject Leg  ;
+    public GameObject Knee ;
 
-    public int x_value ;
-    public int y_value ;
-    public int z_value ;
+    // Hip (upper leg) Variables //
+    public int x_value_hip ;
+    public int y_value_hip ;
+    public int z_value_hip ;
 
-    private int off_x ;
-    private int off_y ;
-    private int off_z ;
+    private int off_x_hip ;
+    private int off_y_hip ;
+    private int off_z_hip ;
+
+
+    // Knee (lower leg) Variables //
+    public int x_value_knee ;
+    public int y_value_knee ;
+    public int z_value_knee ;
+
+    private int off_x_knee ;
+    private int off_y_knee ;
+    private int off_z_knee ;
+
 
 
     void Start()
@@ -31,11 +44,16 @@ public class ALL_MPU_Knee_Biomechanics : MonoBehaviour
         stream.Open();
         
         //Vector3 eulerRotation = Leg.transform.eulerAngles;
-        Vector3 eulerRotation = Leg.transform.localEulerAngles;
+        Vector3 eulerRotation_hip =  Leg.transform.localEulerAngles  ;
+        Vector3 eulerRotation_knee = Knee.transform.localEulerAngles ;
 
-        off_x = (int)eulerRotation.x ;
-        off_y = (int)eulerRotation.y ;
-        off_z = (int)eulerRotation.z ;
+        off_x_hip = (int)eulerRotation_hip.x ;
+        off_y_hip = (int)eulerRotation_hip.y ;
+        off_z_hip = (int)eulerRotation_hip.z ;
+
+        off_x_knee = (int)eulerRotation_knee.x ;
+        off_y_knee = (int)eulerRotation_knee.y ;
+        off_z_knee = (int)eulerRotation_knee.z ;
 
     }
 
@@ -49,20 +67,21 @@ public class ALL_MPU_Knee_Biomechanics : MonoBehaviour
         string[] data = receivedstring.Split(',');
 
         data_received[0] = data[0];
-        int.TryParse(data[0], out x_value);
+        int.TryParse(data[0], out x_value_hip);
 
         data_received[1] = data[1];
-        int.TryParse(data[1], out y_value);
+        int.TryParse(data[1], out y_value_hip);
 
         data_received[2] = data[2];
-        int.TryParse(data[2], out z_value);
+        int.TryParse(data[2], out z_value_hip);
 
 
         //Cube.transform.rotation = Quaternion.Euler((x_value + off_x), (y_value + off_y), (z_value + off_z));
-        
         //Leg.transform.rotation = Quaternion.Euler((x_value + off_x), (y_value + off_y), (z_value + off_z));
 
-        Leg.transform.localEulerAngles = new Vector3((-x_value + off_x), (y_value + off_y), (-z_value + off_z));
+        Leg.transform.localEulerAngles = new Vector3((-x_value_hip + off_x_hip), (y_value_hip + off_y_hip), (-z_value_hip + off_z_hip));
+
+        //Leg.transform.localEulerAngles = new Vector3((-x_value + off_x), (y_value + off_y), (-z_value + off_z));
 
     }
 }
