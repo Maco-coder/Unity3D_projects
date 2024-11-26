34c60ff162bfe8a9a491be7b15a527a18044c44a
Maco-coder <mamoranl@uwaterloo.ca>
2024-11-25 20:56:35 -0500

    Change made

--- Knee Project/HTCVive_Unity_VR_Integration/Assets/Scripts/Rotation_NewBehaviourScript.cs
+++ Knee Project/HTCVive_Unity_VR_Integration/Assets/Scripts/Rotation_NewBehaviourScript.cs
@@ -5,23 +5,36 @@ using UnityEngine               ;
 public class Rotation_NewBehaviourScript : MonoBehaviour
 {
 
-    public GameObject viveTracker1;
-    public GameObject knee        ;
+    public GameObject viveTracker1 ;
+    public GameObject knee         ;
 
-    public int offset_x_tracker1;
-    public int offset_y_tracker1;
-    public int offset_z_tracker1;
+    float offset_x_tracker1 = 92.0f ;
+    public float offset_y_tracker1 ;
+    public float offset_z_tracker1 ;
+
+    float offset_x_knee = 1.07f ;
+    public float offset_y_knee ;
+    public float offset_z_knee ;
+
 
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        if (viveTracker1 != null)
-        {
-            transform.rotation = viveTracker1.transform.rotation;
-        }   
+
+        Vector3 rotationViveTracker1 = viveTracker1.transform.localEulerAngles;
+        Vector3 rotationKnee = knee.transform.localEulerAngles;
+
+        rotationKnee.x = -(rotationViveTracker1.x + (offset_x_tracker1 - offset_x_knee));
+
+        knee.transform.localEulerAngles = rotationKnee;
+
+        float rotationX = knee.transform.localEulerAngles.x;
+
+        Debug.Log("Knee X Rotation: " + rotationX.ToString("F2"));
+
     }
 }
