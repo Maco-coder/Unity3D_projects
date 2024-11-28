320e08023d1e628495fcfc312a9f33b6c89a95fd
Maco-coder <mamoranl@uwaterloo.ca>
2024-11-27 21:14:25 -0500

    Change made

--- Knee Project/HTCVive_Unity_VR_Integration/Assets/Quaternion_rotation_trackers.cs
+++ Knee Project/HTCVive_Unity_VR_Integration/Assets/Quaternion_rotation_trackers.cs
@@ -5,9 +5,12 @@ using UnityEngine               ;
 public class Quaternion_rotation_trackers : MonoBehaviour
 {
 
+    public GameObject knee ;
+
     public Transform tracker1   ;
     public Transform tracker2   ;
     public Transform virtualKnee;
+
 
     void Start()
     {
@@ -17,13 +20,34 @@ public class Quaternion_rotation_trackers : MonoBehaviour
     void Update()
     {
         // Get the rotation of both trackers (in world space)
-        Quaternion VIVErotation1 = tracker1.rotation;
-        Quaternion VIVErotation2 = tracker2.rotation;
+        Quaternion VIVErotation1 = tracker1.rotation ;
+        Quaternion VIVErotation2 = tracker2.rotation ;
 
         // Calculate the relative rotation between the two trackers
         Quaternion relativeRotation = Quaternion.Inverse(VIVErotation1)*VIVErotation2 ;
+        Vector3 relativeEulerAngles = relativeRotation.eulerAngles                    ;
+
+        Vector3 eulerRotationKnee = knee.transform.localEulerAngles ;
+        eulerRotationKnee.x = NormalizeAngle(relativeEulerAngles.x) ;
+        //eulerRotationKnee.y = NormalizeAngle(relativeEulerAngles.y) ;
+        eulerRotationKnee.z = NormalizeAngle(relativeEulerAngles.z) - 32.0f ;
 
         // Apply the relative rotation to the virtual knee
-        virtualKnee.rotation = relativeRotation ;
+        
+        virtualKnee.rotation = relativeRotation            ;
+        knee.transform.localEulerAngles = eulerRotationKnee;
+
+        Debug.Log("X Rotation: " + eulerRotationKnee.x.ToString("F2") + " Tracker Y Rotation: " + eulerRotationKnee.y.ToString("F2"));
     }
+
+
+    float NormalizeAngle(float angle)
+    {
+        if (angle > 180)
+            angle -= 360;
+        else if (angle < -180)
+            angle += 360;
+        return angle;
+    }
+
 }
