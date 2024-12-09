4e047a3016c1d1b0d0c24cce0f5e4f0d70c1cd8f
Maco-coder <mamoranl@uwaterloo.ca>
2024-12-09 17:27:59 -0500

    Change made

--- Knee Project/HTCVive_Unity_VR_Integration/Assets/Scripts/Knee_Physical_Virtual_Collocation.cs
+++ Knee Project/HTCVive_Unity_VR_Integration/Assets/Scripts/Knee_Physical_Virtual_Collocation.cs
@@ -1,17 +1,18 @@
-using System.Collections        ;
-using System.Collections.Generic;
-using UnityEngine               ;
+using System.Collections         ;
+using System.Collections.Generic ;
+using UnityEngine                ;
 
 public class Knee_Physical_Virtual_Collocation : MonoBehaviour
 {
 
-    public GameObject manikin           ;
+    public GameObject manikin ;
+    public GameObject hip1    ;
 
     public Transform tracker1           ;
     private bool hasInitialized = false ;
     
-    private Vector3 initialTrackerPosition   ;
-    private Quaternion initialTrackerRotation;
+    private Vector3 initialTrackerPosition     ;
+    private Quaternion initialTracker1Rotation ;
 
 
     void Start()
@@ -22,20 +23,41 @@ public class Knee_Physical_Virtual_Collocation : MonoBehaviour
     void Update()
     {
 
-        if (!hasInitialized && tracker1 != null)
+        //Quaternion VIVErotation1 = tracker1.rotation ;
+
+        if (hasInitialized == false && tracker1 != null)
         {
-            initialTrackerPosition = tracker1.position;
-            initialTrackerRotation = tracker1.rotation;
-            hasInitialized = true                    ;
+            initialTrackerPosition = tracker1.position  ;
+            initialTracker1Rotation = tracker1.rotation ;
+            manikin.transform.position = initialTrackerPosition;
+            hasInitialized = true ;
         }
 
-        if (hasInitialized && tracker1 != null)
+        if (hasInitialized == true && tracker1 != null)
         {
 
-            Debug.Log("Initial Tracker Position: " + initialTrackerPosition) ;
-            manikin.transform.position = initialTrackerPosition ;
-            manikin.transform.rotation = initialTrackerRotation ;
+            Debug.Log("Initial Tracker Position: " + hasInitialized) ;
+            //manikin.transform.position = initialTrackerPosition ;
+
+            //Quaternion currentTracker1Rotation = tracker1.rotation ;
+            //Quaternion relativeInitialRotation1 = Quaternion.Inverse(initialTracker1Rotation) * currentTracker1Rotation ;
+            //Vector3 relativeEurlerAngles_Hip1 = relativeInitialRotation1.eulerAngles ;
+            //Vector3 eulerRotationHip1 = hip1.transform.localEulerAngles              ;
+            //eulerRotationHip1.x = NormalizeAngles(relativeEurlerAngles_Hip1.x)       ;
+            //eulerRotationHip1.y = NormalizeAngles(relativeEurlerAngles_Hip1.y) ;
+            //eulerRotationHip1.z = NormalizeAngles(relativeEurlerAngles_Hip1.z) ;
+
+            //hip1.transform.localEulerAngles = eulerRotationHip1 ;
 
         }
+    }
+
+    float NormalizeAngles(float angles)
+    {
+        if (angles > 180)
+            angles -= -360;
+        else if (angles < -180)
+            angles += 360;
+        return angles;
     }
 }
