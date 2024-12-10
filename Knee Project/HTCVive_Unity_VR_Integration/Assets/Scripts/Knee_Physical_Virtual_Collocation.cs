59e8c0686486782964dabef541ed0aa7a9ed1160
Maco-coder <mamoranl@uwaterloo.ca>
2024-12-10 11:31:15 -0500

    Change made

--- Knee Project/HTCVive_Unity_VR_Integration/Assets/Scripts/Knee_Physical_Virtual_Collocation.cs
+++ Knee Project/HTCVive_Unity_VR_Integration/Assets/Scripts/Knee_Physical_Virtual_Collocation.cs
@@ -7,11 +7,14 @@ public class Knee_Physical_Virtual_Collocation : MonoBehaviour
 
     public GameObject manikin ;
     public GameObject hip1    ;
+    public GameObject knee1   ;
 
-    public Transform tracker1           ;
+    public Transform tracker1 ;   // Upper Leg Tracker
+    public Transform tracker2 ;   // Lower Leg Tracker
+
     private bool hasInitialized = false ;
     
-    private Vector3 initialTrackerPosition     ;
+    private Vector3 initialTracker1Position    ;
     private Quaternion initialTracker1Rotation ;
 
 
@@ -23,31 +26,45 @@ public class Knee_Physical_Virtual_Collocation : MonoBehaviour
     void Update()
     {
 
-        //Quaternion VIVErotation1 = tracker1.rotation ;
+        Quaternion VIVErotation1 = tracker1.rotation ;
+        Quaternion VIVErotation2 = tracker2.rotation ;
 
         if (hasInitialized == false && tracker1 != null)
         {
-            initialTrackerPosition = tracker1.position  ;
+            initialTracker1Position = tracker1.position ;
             initialTracker1Rotation = tracker1.rotation ;
-            manikin.transform.position = initialTrackerPosition;
+            manikin.transform.position = initialTracker1Position ;
+            manikin.transform.rotation = initialTracker1Rotation ;
             hasInitialized = true ;
         }
 
         if (hasInitialized == true && tracker1 != null)
         {
 
-            Debug.Log("Initial Tracker Position: " + hasInitialized) ;
-            //manikin.transform.position = initialTrackerPosition ;
+            // HIP JOINT KINEMATICS //
 
-            //Quaternion currentTracker1Rotation = tracker1.rotation ;
-            //Quaternion relativeInitialRotation1 = Quaternion.Inverse(initialTracker1Rotation) * currentTracker1Rotation ;
-            //Vector3 relativeEurlerAngles_Hip1 = relativeInitialRotation1.eulerAngles ;
-            //Vector3 eulerRotationHip1 = hip1.transform.localEulerAngles              ;
-            //eulerRotationHip1.x = NormalizeAngles(relativeEurlerAngles_Hip1.x)       ;
-            //eulerRotationHip1.y = NormalizeAngles(relativeEurlerAngles_Hip1.y) ;
-            //eulerRotationHip1.z = NormalizeAngles(relativeEurlerAngles_Hip1.z) ;
+            Quaternion currentTracker1Rotation = tracker1.rotation ;
+            Quaternion relativeInitialRotation1 = Quaternion.Inverse(initialTracker1Rotation) * currentTracker1Rotation ;
+            Vector3 relativeEurlerAngles_Hip1 = relativeInitialRotation1.eulerAngles ;
+            Vector3 eulerRotationHip1 = hip1.transform.localEulerAngles              ;
+            eulerRotationHip1.x = NormalizeAngles(relativeEurlerAngles_Hip1.x)       ;
+            eulerRotationHip1.y = - NormalizeAngles(relativeEurlerAngles_Hip1.y)     ;
+            eulerRotationHip1.z = 180 + NormalizeAngles(relativeEurlerAngles_Hip1.z) ;
 
-            //hip1.transform.localEulerAngles = eulerRotationHip1 ;
+            hip1.transform.localEulerAngles = eulerRotationHip1 ;
+
+
+            // KNEE JOINT KINEMATICS //
+
+            Quaternion relativeRotation = Quaternion.Inverse(VIVErotation2) * VIVErotation1 ;
+            Vector3 relativeEulerAngles_knee = relativeRotation.eulerAngles ;
+
+            Vector3 eulerRotationKnee = knee1.transform.localEulerAngles ;
+            eulerRotationKnee.x = NormalizeAngles(relativeEulerAngles_knee.x) - 0.540f  ;
+            //eulerRotationKnee.y = NormalizeAngles(relativeEulerAngles_knee.y) ;
+            eulerRotationKnee.z = - NormalizeAngles(relativeEulerAngles_knee.z) + 0.60f ;
+            knee1.transform.localEulerAngles = eulerRotationKnee ;
+
 
         }
     }
