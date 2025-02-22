7a719d2b9d7d20d8ed57b1a8a1f934c686a49266
Maco-coder <71836273+Maco-coder@users.noreply.github.com>
2025-02-22 15:32:41 -0500

    Change made

---
+++ Knee Project/My project (1)/Assets/Scripts/Script_Offsets.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_Offsets : MonoBehaviour
+{
+
+    public GameObject Hip_reference;
+    public GameObject Manikin_body ;
+
+    public float offset_position_x ;
+    public float offset_position_y ;
+    public float offset_position_z ;
+
+    public float offset_angle_x ;
+    public float offset_angle_y ;
+    public float offset_angle_z ;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
