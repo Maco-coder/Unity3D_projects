51b7f8f5f67fce5275fcb5d06948c9648d9057cf
Maco-coder <mamoranl@uwaterloo.ca>
2025-04-23 17:07:25 -0400

    Change made

---
+++ Knee Project/Hand_placement_FSR_Oculus/Hand_placement_FSR_Oculus.ino
@@ -0,0 +1,42 @@
+
+const int sensor1 = A0 ;
+const int sensor2 = A1 ;
+
+int valSensor1 = 0 ;
+int valSensor2 = 0 ;
+
+const int LED_sensor1 = 3 ;
+const int LED_sensor2 = 4 ;
+
+const int interval = 100   ;
+unsigned long prevTime = 0 ;
+
+
+
+void setup() {
+
+  Serial.begin(38400);
+  pinMode(LED_sensor1, OUTPUT) ;
+  pinMode(LED_sensor2, OUTPUT) ;
+  
+}
+
+
+void loop() {
+
+  unsigned long currentTime = millis();
+
+  valSensor1 = analogRead(sensor1) ;
+  valSensor2 = analogRead(sensor2) ;
+
+  if (currentTime - prevTime){
+
+    Serial.print(valSensor1)   ;
+    Serial.print(", ")         ;
+    Serial.println(valSensor2) ;
+
+    prevTime = currentTime   ;
+    
+  }
+
+}
