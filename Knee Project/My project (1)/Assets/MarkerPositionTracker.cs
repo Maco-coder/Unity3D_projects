41856e13c4cad8cfd62895c9e1ae1a18aefa96e7
Maco-coder <71836273+Maco-coder@users.noreply.github.com>
2025-04-26 10:39:57 -0400

    Change made

--- Knee Project/My project (1)/Assets/MarkerPositionTracker.cs
+++ Knee Project/My project (1)/Assets/MarkerPositionTracker.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using OptiTrack;
+using NaturalPoint;
+using NaturalPoint.NatNetLib;
+
 
 public class MarkerPositionTracker : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    public OptiTrackStreamingClient StreamingClient;
+    public Transform trackedObject;
+
+    
     void Update()
     {
-        
+
+        if (StreamingClient != null && StreamingClient.IsConnected)
+        {
+            UnlabeledMarker[] unlabeledMarkers = StreamingClient.GetUnlabeledMarkers();
+
+            if (unlabeledMarkers.Length > 0)
+            {
+                // Get the position of the first unlabeled marker
+                Vector3 markerPosition = unlabeledMarkers[0].Position;
+
+                // OptiTrack coordinates are often in meters, Unity in world units
+                // You might need to adjust the scale and coordinate system
+                if (trackedObject != null)
+                {
+                    trackedObject.position = markerPosition;
+                }
+                else
+                {
+                    Debug.Log("Marker Position: " + markerPosition);
+                }
+            }
+            else
+            {
+                Debug.Log("No unlabeled markers detected.");
+            }
+        }
+        else
+        {
+            Debug.Log("OptiTrack Streaming Client not connected.");
+        }
+
     }
 }
